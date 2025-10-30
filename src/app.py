fd6cadd44a13e37422f9ec682a9704addd320ac8
root <root@LAB-DEVNET01.homelabcompany.local>
2025-10-30 09:36:00 -0400

    New and improved app.py code after linting with pylint.

--- src/app.py
+++ src/app.py
@@ -5,19 +5,18 @@ Purpose: This is a lab web app to be used throughout the DevOps labs within the
 Cisco Certified DevNet Associate course.
 """
 
-
-from flask import Flask, jsonify
 import json
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    with open('db.json') as file:
+    """Return data from the db.json file over the webpage."""
+    with open('db.json', encoding='utf-8') as file:
         data = json.load(file)
     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
